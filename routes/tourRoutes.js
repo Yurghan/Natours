b773a7dc29275f69259ef48c7bd32a4b9a1aa30b
Miłosz Topolewski <topolewski.webmilo@gmail.com>
2022-05-26 18:47:14 +0200

    first

---
+++ routes/tourRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const tourController = require('../controllers/tourController');
+
+const router = express.Router();
+
+router.param('id', tourController.checkID);
+router.param('name, price', tourController.checkBody);
+
+// Create a checkBody middleware
+
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
